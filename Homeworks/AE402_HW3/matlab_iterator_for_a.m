f0355ce2bddbc0705497d7398a56614e490913ca
Sebastian Rimmer <sebr587@gmail.com>
2023-10-12 17:19:01 +0200

    edited for jupiter transfer case

--- Homeworks/AE402_HW3/matlab_iterator_for_a.m
+++ Homeworks/AE402_HW3/matlab_iterator_for_a.m
@@ -1,20 +1,20 @@
 % Lambert Solver for HW3 Problem_1 - Earth-Apophis Intercept
 
-clear all; 
+clear all;
 close all; clc;
-
-a = fzero(@lambert, 1.1)
 
 function f = lambert(a)
 
-s  = 1.8381;        % Pre-calculated space-triangle semi-perimeter
-c  = 1.6007;        % Pre-calculated chord length
-tf = (410/365.25) * 2 * pi;     % ToF of 410 days
+s  = 6.13;        % Pre-calculated space-triangle semi-perimeter
+c  = 6.06;        % Pre-calculated chord length
+tf = (524/365.25) * 2 * pi;     % ToF of 410 days
 
-% Updated value for alpha because our t_f is greater than t_m 
-alpha = 2*pi - 2*asin(sqrt(s/(2*a)));
-beta  = - 2*asin(sqrt((s-c)/(2*a)));
+% Updated value for alpha because our t_f is greater than t_m
+alpha = 2*asin(sqrt(s/(2*a)));
+beta  = 2*asin(sqrt((s-c)/(2*a)));
 
 f = tf-(a^(3/2))*(alpha-beta-sin(alpha)+sin(beta));
 
 end
+
+a = fzero(@lambert, 5)
