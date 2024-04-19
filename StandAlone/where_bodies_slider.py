ff305208e3479dbc63c3f01a56e26ab74b47e7b2
Sebastian Rimmer <sebr587@gmail.com>
2024-04-19 16:19:03 +0200

    Planets slider first draft

--- StandAlone/where_bodies_slider.py
+++ StandAlone/where_bodies_slider.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
@@ -10,7 +11,12 @@ import json
 import datetime
 import jpl_horizons_planets_mapper_copy
 
-def update(slider, scat_e, x_e, y_e, scat_d, x_d, y_d,scat_m, x_m, y_m):
+def update(slider, scat_e, x_e, y_e, scat_d, x_d, y_d,scat_m, x_m, y_m, text_annotation):
+
+
+    date_0 = datetime.datetime(2019, 10, 26)
+    selected_date = date_0 + datetime.timedelta(days=int(slider))
+    text_annotation.set_text(selected_date.strftime("%Y-%m-%d"))
 
     frame = int(slider)
 
@@ -53,9 +59,9 @@ def main():
     
     mu_sun = float(1.32712440018E+11)   
     start = datetime.datetime.strptime(earth['start_time'], '%Y-%b-%d %H:%M:%S.%f')
-    end = datetime.datetime(2026, 12, 16)
+    end = datetime.datetime(2028, 12, 16)
 
-    dt_days = (end - start).days
+    dt_days = (end - start).days + 1
     r0_vector_earth,  v0_vector_earth = np.array([earth['X'], earth['Y'], earth['Z']]), np.array([earth['VX'], earth['VY'], earth['VZ']])
     r0_vector_didymos,  v0_vector_didymos = np.array([didymos['X'], didymos['Y'], didymos['Z']]), np.array([didymos['VX'], didymos['VY'], didymos['VZ']])
     r0_vector_mars,  v0_vector_mars = np.array([mars['X'], mars['Y'], mars['Z']]), np.array([mars['VX'], mars['VY'], mars['VZ']])
@@ -63,7 +69,7 @@ def main():
     x_e, y_e = [], []
     x_d, y_d = [], []
     x_m, y_m = [], []
-
+    theta_j2000_e, theta_j2000_d = np.array([]), np.array([])
     for i in range(dt_days):
         
         r1_vector_e, v1_vector_e = jpl_horizons_planets_mapper_copy.r_and_v_as_function_of_t(mu_sun, r0_vector_earth, v0_vector_earth, 24*3600)
@@ -80,7 +86,26 @@ def main():
         r0_vector_earth, v0_vector_earth = r1_vector_e, v1_vector_e
         r0_vector_didymos, v0_vector_didymos = r1_vector_d, v1_vector_d
         r0_vector_mars, v0_vector_mars = r1_vector_m, v1_vector_m
-    
+
+        # true anomalies
+        f_e, f_d = jpl_horizons_planets_mapper_copy.true_anom_f(r1_vector_e, v1_vector_e, mu_sun), jpl_horizons_planets_mapper_copy.true_anom_f(r1_vector_d, v1_vector_d, mu_sun)
+        w_e, w_d = jpl_horizons_planets_mapper_copy.arg_of_periapse(r1_vector_e, v1_vector_e, mu_sun), jpl_horizons_planets_mapper_copy.arg_of_periapse(r1_vector_d, v1_vector_d, mu_sun)
+        Ohm_e, Ohm_d = jpl_horizons_planets_mapper_copy.ra_o_an(r1_vector_e, v1_vector_e), jpl_horizons_planets_mapper_copy.ra_o_an(r1_vector_d, v1_vector_d)
+
+        # theta in ecliptic j2000 frame, for reference
+        sum_e = f_e + w_e + Ohm_e -720
+        if sum_e < 0:
+            sum_e += 360
+
+        sum_d = f_d + w_d + Ohm_d
+        if sum_d > 360:
+            sum_d -= 360
+        if sum_d > 360:
+            sum_d -= 360
+
+        theta_j2000_e = np.append(theta_j2000_e, sum_e)
+        theta_j2000_d = np.append(theta_j2000_d, sum_d)
+
     x_e, y_e, = np.array(x_e), np.array(y_e)
     x_d, y_d, = np.array(x_d), np.array(y_d)
     x_m, y_m, = np.array(x_m), np.array(y_m)
@@ -105,15 +130,63 @@ def main():
     scat_e = ax.scatter(x_e[0], y_e[0], color='blue')
     scat_d = ax.scatter(x_d[0], y_d[0], color='gray')
     scat_m = ax.scatter(x_m[0], y_m[0], color='orange')
-
+   
     # Slider bullshits
     # ----------------------------
-    ax_slider = plt.axes([0.2, 0.1, 0.65, 0.03], facecolor='lightgoldenrodyellow')
-    sDays = Slider(ax_slider, 'Days', 0, 735, valinit=735, valstep=1) # this needs to be initialised at 735 to fit in full range
-    sDays.on_changed(lambda val: update(val, scat_e, x_e, y_e, scat_d, x_d, y_d, scat_m, x_m, y_m))
+    ax_slider = plt.axes([0.15, 0.1, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+    sDays = Slider(ax_slider, 'Date', 0, dt_days, valinit=dt_days, valstep=1)
+
+    # Add a text annotation to display the selected date
+    text_annotation = ax_slider.text(1.05, 0.5, '', transform=ax_slider.transAxes, va='center')
+    text_annotation.set_text((start + datetime.timedelta(days=int(sDays.val))).strftime("%Y-%m-%d"))
+
+    sDays.on_changed(lambda val: update(val, scat_e, x_e, y_e, scat_d, x_d, y_d, scat_m, x_m, y_m, text_annotation))
+    sDays.valtext.set_visible(False)
+
+    # Calculating the distance for transfer opps
+    # ----------------------------
+    # Comparing circular orbit with time period of elliptical orbit. For visual sake, lets just plot sun distances for now
+    plt.figure()
+
+    separation_theta = np.array([])
+    for i in range(dt_days):
+        
+        xi_e, yi_e = x_e[i], y_e[i] 
+        xi_d, yi_d = x_d[i], y_d[i]
+   
+        r_earth_theta = np.sqrt(xi_e**2 + yi_e**2)
+        r_didymos_theta = np.sqrt(xi_d**2 + yi_d**2)
+        r_diff = np.sqrt(abs(xi_e-xi_d)**2 + abs(yi_e-yi_d)**2)
+        theta_diff = (r_earth_theta**2 + r_didymos_theta**2 -  r_diff**2) / ( 2 * r_earth_theta * r_didymos_theta)
+        theta_diff = np.arccos(theta_diff) * 180/np.pi
+
+        separation_theta = np.append(separation_theta, theta_diff)
+
+    plt.plot(pd.date_range(start=start, end=end, freq='D'), separation_theta)
+
+    # for i in range(1, 10):
+    #     plt.plot([365*i, 365*i], [0, 180], 'red')
+    plt.grid(True)
+    plt.ylim([0, 180])
+    
+   # Set x-axis label format
+    plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y-%m'))
+    plt.gca().xaxis.set_major_locator(plt.matplotlib.dates.MonthLocator(interval=3))
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45)
+
+    # Add labels and title
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.title('Earth-Didymos separation angle')
+
 
     plt.show()
 
+    # To be udapted to be representative of something similar too ... 
+    # https://stackoverflow.com/questions/44985966/managing-dynamic-plotting-in-matplotlib-animation-module/44989063#44989063
+    
     return 0
 
 if __name__ == '__main__':
